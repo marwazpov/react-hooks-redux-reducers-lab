e114a18b238c810dd7eb6ba5de8756bc859372ea
marwa <mds08@mail.aub.edu>
2022-09-30 18:46:34 +0300

    done

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,28 @@
-export function manageFriends(state, action) {
-  // your code here
+const initialState ={
+  friends: [],
+};
+
+
+export function manageFriends(state=initialState, action) {
+  switch (action.type){
+    case "friends/add":{
+      return{
+        ...state,
+        friends:[
+          ...state.friends,{
+            name:action.payload.name,
+            hometown:action.payload.hometown,
+            id:action.payload.id,
+          }
+        ]
+      };
+    }
+    case "friends/remove":
+    return{
+      ...state,
+      friends: state.friends.filter((friend)=>friend.id !== action.payload),
+    };
+    default:
+      return state;
+  }
 }
--- src/managePresents.js
+++ src/managePresents.js
@@ -1,5 +1,16 @@
-const initialState = { numberOfPresents: 0 };
+const initialState = {
+  numberOfPresents: 0
+};
+
 
 export function managePresents(state = initialState, action) {
-  // your code here
+  switch (action.type){
+    case "presents/increase":
+      return {
+        ...state,
+        numberOfPresents: state.numberOfPresents + 1,
+      };
+    default:
+      return state;
+  }
 }
